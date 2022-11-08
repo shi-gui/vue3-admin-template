a5c3d56770b2f0dadea08bc609ba70854ebb6bd6
liuhua <1670835059@qq.com>
2022-11-09 00:17:50 +0800

    ci: 添加less，并删除无用代码

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>about页面</h1>
+    <div>
+      <p>12121</p>
+      <span>23232</span>
+    </div>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<style lang="less" scoped>
+div {
+  p {
+    color: red;
+  }
+  span {
+    color: green;
   }
 }
 </style>
